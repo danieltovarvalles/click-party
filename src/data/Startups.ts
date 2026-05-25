dc6043f1bb8ab0a83a93db377e74bc3b36ae33c9
Daniel <danieltovarvalles@gmail.com>
2026-05-24 22:12:42 -0600

    Update data from startups

--- src/data/Startups.ts
+++ src/data/Startups.ts
@@ -7,6 +7,12 @@ import logoFiestaPlus from "../assets/startups/FiestaPlus_Logo.jpeg";
 import coverFiestaPlus from "../assets/startups/FiestaPlus_Photo.jpg";
 import logoAltagracia from "../assets/startups/Altagracia_Logo.jpg";
 import coverAltagracia from "../assets/startups/Altagracia_Cover.jpeg";
+import logoMBBites from "../assets/startups/M-B-Bites-Logo.svg"
+import coverMBBites from "../assets/startups/M-B-Bites_cover.jpeg"
+import logoBaluna from "../assets/startups/Baluna_logo.jpeg"
+import coverBaluna from "../assets/startups/Baluna_cover.jpeg"
+import logoJassania from "../assets/startups/Jassania_logo.jpeg"
+
 
 export type ImageOrIcon = ImageMetadata | typeof logo9293Craft;
 
@@ -18,23 +24,21 @@ export type LogoPresentation = {
 export type Startup = {
   id: number;
   name: string;
-  slug: string;
-  description: string;
-  image: ImageOrIcon;
+  description?: string;
+  image?: ImageOrIcon;
   logo?: ImageOrIcon;
   logoPresentation?: LogoPresentation;
   phone?: string;
   facebook?: string;
   whatsappUrl?: string;
   mainPageUrl?: string;
-  categoryIds: number[];
+  categoryIds?: number[];
 };
 
 export const startups: Startup[] = [
   {
     id: 1,
     name: "9293 Craft",
-    slug: "9293craft",
     description: "Corte y grabado laser, recuerdos, decoración.",
     image: cover9293Craft,
     logo: logo9293Craft,
@@ -43,12 +47,10 @@ export const startups: Startup[] = [
     },
     phone: "528711068211",
     mainPageUrl: "https://www.instagram.com/9293craft/",
-    categoryIds: [1, 2, 3, 4, 5, 6, 7, 8],
   },
   {
     id: 2,
     name: "BANED",
-    slug: "banned",
     description: "Hielitos,cupcakes y postres gourmet.",
     image: coverBaned,
     logo: logoBaned,
@@ -56,27 +58,35 @@ export const startups: Startup[] = [
       imageClass: "p-1",
     },
     phone: "528714150303",
-    categoryIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  },
+  {
+    id: 5,
+    name: "M & B Bites",
+    description: "Snack bar y decoraciones.",
+    image: coverMBBites,
+    logo: logoMBBites,
+    logoPresentation: {
+      imageClass: "p-2",
+    },
+    phone: "528715683714",
+    mainPageUrl: "https://www.facebook.com/profile.php?id=61587287816267",
   },
   {
     id: 3,
     name: "Fiesta Plus",
-    slug: "fiesta-plus",
     description: "Renta de sillas, mesas y brincolin.",
     image: coverFiestaPlus,
     logo: logoFiestaPlus,
     logoPresentation: {
-      imageClass: "scale-[1.7] pt-1",
+      imageClass: "scale-[1.7] pt-[6px]",
     },
     facebook: "100065216013301",
     mainPageUrl: "https://www.facebook.com/share/1GkLakqAx2/?mibextid=wwXIfr",
-    categoryIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   },
   {
     id: 4,
     name: "Altagracia Eventos",
-    slug: "altagracia",
-    description: "Renta de sillas, mesas y brincolin.",
+    description: "Decoración, animación, snacks, mobiliario y mucho más.",
     image: coverAltagracia,
     logo: logoAltagracia,
     logoPresentation: {
@@ -84,6 +94,25 @@ export const startups: Startup[] = [
     },
     phone: "528721373025",
     mainPageUrl: "https://www.facebook.com/profile.php?id=61554953079082",
-    categoryIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  },
+  {
+    id: 6,
+    name: "Baluna",
+    description: "Personalizados creativos para eventos, regalos y momentos especiales.",
+    image: coverBaluna,
+    logo: logoBaluna,
+    phone: "528717962675",
+    mainPageUrl: "https://www.instagram.com/baluna.creativecrafts?igsh=MWYxMDQ5bGVxdDVkaQ%3D%3D&utm_source=qr"
+  },
+  {
+    id: 7,
+    name: "Jassania",
+    description: "Decoración, flores.",
+    logo: logoJassania,
+    // logoPresentation: {
+    //   imageClass: "scale-110",
+    // },
+    // phone: "528721373025",
+    // mainPageUrl: "https://www.facebook.com/profile.php?id=61554953079082",
   },
 ];
